056d9be0b6cc85ebd2fc6d46a07a4d7184cb0dd9
wajih942 <73997820+wajih942@users.noreply.github.com>
2022-09-09 18:19:01 +0100

    commit

--- Messenger/Model/RealmMessages/LocalMessage.swift
+++ Messenger/Model/RealmMessages/LocalMessage.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import MessageKit
+import RealmSwift
+
+class LocalMessage : Object, Codable {
+    @objc dynamic var id = ""
+    @objc dynamic var chatRoomId = ""
+    @objc dynamic var date = Date()
+    @objc dynamic var senderName = ""
+    @objc dynamic var id = ""
+    
+}
